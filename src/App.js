be6355f658fe4db93931455597cd851cb4d587f3
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-02-17 11:36:23 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Calc from './Component/Calc';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Calc/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
